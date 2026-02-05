615ca60cfa4adeb6298d4c0b2f39c194ebfc33e0
Dima Zakharov <dzakarov@gmail.com>
2026-02-05 15:09:25 +0000

    test

--- sse/test-init-sse.sh
+++ sse/test-init-sse.sh
@@ -8,13 +8,12 @@ URL_INIT="${URL}/sse"
 
 SESSION="mcp-time-server$PORT"
 
-tmux kill-session -t $SESSION || echo session not found 
+tmux kill-session -t $SESSION || echo session not found
 
 tmux new-session -d -s $SESSION \
-  "curl -N -X GET -H \"Content-Type: application/json\" -H \"Accept: application/json, text/event-stream\" $URL_INIT | tee sse.txt"
+	"curl -N -X GET -H \"Content-Type: application/json\" -H \"Accept: application/json, text/event-stream\" $URL_INIT | tee sse.txt"
 
 sleep .1
-
 
 URL_MSG="$URL$(sed -n 's/^data: //p' sse.txt | tr -d '\r' | head -n 1)"
 
@@ -26,16 +25,14 @@ NOTIFY='{"jsonrpc": "2.0","method": "notifications/initialized"}'
 LIST='{"jsonrpc":"2.0","id":2,"method":"tools/list"}'
 CALL='{"jsonrpc":"2.0","id":3,"method":"tools/call","params":{"name":"get_system_time","arguments":{"timezone":"UTC"}}}'
 
-
-curl "$URL_MSG"  -d "$INIT"
-curl "$URL_MSG"  -d "$NOTIFY"
-curl  "$URL_MSG"  -d "$LIST"
-curl  "$URL_MSG"  -d "$CALL"
-
+curl "$URL_MSG" -d "$INIT"
+curl "$URL_MSG" -d "$NOTIFY"
+curl "$URL_MSG" -d "$LIST"
+for i in $(seq 1 10); do
+	curl "$URL_MSG" -d "$CALL"
+	sleep .01
+done
 
 cat sse.txt
 
-
 tmux kill-session -t $SESSION
-
-
